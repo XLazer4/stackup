1704556a20b07445f1daf7ce5a462deaba29c2b4
XLazer4 <khannarahul446@gmail.com>
2023-01-24 12:24:25 +0530

    react blog

---
+++ blog-site/src/author.js
@@ -0,0 +1,7 @@
+export const about_author = {
+  author_1: {
+    title: "About the Author: XLazer",
+    content:
+      "I have been interested in web development for quite some time now. It is more of a necessity for me than interest tbh, I have been coding in the web3 space for quite some time now, mainly smart contracts, thing is I have to have a frontend for to show it to the world. Hence web dev became a must have for me and stackup is helping me to get closer this goal!",
+  },
+};
